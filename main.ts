a448bc36d99c8394c39182b9d26a3702eb8784d2
mrkaosha <113385802+mrkaosha@users.noreply.github.com>
2022-09-14 16:53:04 -0700

    now having a caboose at the end works!

--- main.ts
+++ main.ts
@@ -31,6 +31,24 @@ let eatenflag = 0
 scene.setBackgroundImage(assets.image`Grid`)
 eatenflag = 0
 let snake_head = sprites.create(assets.image`snake block`, SpriteKind.Player)
+let snaketail = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    . . . . . . 9 9 9 9 9 9 9 9 9 9 
+    `, SpriteKind.Player)
 let snakefood = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -50,17 +68,27 @@ let snakefood = sprites.create(img`
     . . . . . . 5 5 5 5 5 5 5 5 5 5 
     `, SpriteKind.Food)
 snake_head.setPosition(2, 2)
+snaketail.setPosition(-8, 2)
+let snake_length = 1
+let vx_history = [-99, 1]
+let vy_history = [-99, 0]
 vx = 1
 vy = 0
 foodx = 80
 foody = 60
 game.onUpdate(function () {
     if ((snake_head.x - 2) % 10 == 0 && (snake_head.y - 2) % 10 == 0) {
+        vx_history.shift()
+        vy_history.shift()
         updatevx = vx
         updatevy = vy
+        vx_history.push(updatevx)
+        vy_history.push(updatevy)
     }
     snake_head.x += updatevx
     snake_head.y += updatevy
+    snaketail.x += vx_history[0]
+    snaketail.y += vy_history[0]
     snakefood.setPosition(foodx + 2, foody + 2)
     if (eatenflag >= 2) {
         eatenflag = 0
